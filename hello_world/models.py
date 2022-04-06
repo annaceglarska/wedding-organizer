8ee28db1107221af5f8aa03888ad44301a41a8ee
annaceglarska <aniaceglarska1@gmail.com>
2022-04-06 19:57:21 +0200

    Migration_1

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+title = models.CharField(max_length=250)
--- hello_world/tests/test.py
+++ hello_world/tests/test.py
@@ -3,13 +3,20 @@ from django.urls import reverse
 
 
 class HelloTests(TestCase):
-   def test_contains_hello(self):
-       response = self.client.get(reverse('hello_world_with_name', args={'name': 'Ania'}))
-       self.assertEqual(response.status_code, 200)
-       html = response.getvalue()
-       self.assertEqual(html.find('Ania') > -1, True)
-       self.assertEqual(html.find('Grzegorz') > -1, False)
+    def test_contains_hello(self):
+        args = ['Ania']
+        #response = self.client.get(reverse('hello_world_with_name', args={'name': 'Ania'}))
+        response = self.client.get(reverse('hello_world_with_name', args=args))
+        #self.assertEqual(response.status_code, 200)
+        # html = response.getvalue()
+        # htmlString = html.decode('utf-8')
+        # print(htmlString)
+        # self.assertIn("Hello", htmlString)
+        self.assertContains(response, "Hello", html=False)
+        #self.assertContains(response, "<h1>Hello  Ania</h1>", html=True)
 
-
-
+    def test_contain_url_parameter(self):
+        arg = 'Ania'
+        response = self.client.get(reverse('hello_world_with_name', args= [arg]))
+        self.assertContains(response, arg)
 
